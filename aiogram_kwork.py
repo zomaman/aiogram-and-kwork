792133c5308e4ca13f88dbda1ff50930006e29c3
zomaman <vova.lobko123@gmail.com>
2022-10-22 13:37:46 +0300

    Добавил два основных файла

---
+++ aiogram_kwork.py
@@ -0,0 +1,87 @@
+import asyncio
+from kwork import Kwork
+from config import TOKEN, ADMIN_ID
+from aiogram import Bot, Dispatcher, executor, types
+
+bot = Bot(token=TOKEN)
+loop = asyncio.get_event_loop()
+dp = Dispatcher(bot, loop=loop)
+unread_names_ids = dict()
+
+login_kwork = "Иди нахуй!"
+passwork_kwork = "Иди нахуй!"
+
+
+@dp.message_handler(commands=["start", "help"])
+async def start(message: types.Message):
+    if message.from_user.id == ADMIN_ID:
+        await bot.send_message(message.from_user.id, "Команды: /help /check /authorization")
+    else:
+        await bot.send_message(message.from_user.id, "Команды: /help /check")
+
+
+@dp.message_handler(commands=["check"])
+async def check(message: types.Message):
+    await bot.send_message(message.from_user.id, "Бот работает")
+
+
+@dp.message_handler(commands=["authorization"])
+async def authorization(message: types.Message):
+    if message.from_user.id == ADMIN_ID:
+        await bot.send_message(message.from_user.id, "Отправь логин и пароль через пробел")
+
+        @dp.message_handler(content_types=["text"])
+        async def input(message: types.Message):
+            global login_kwork, passwork_kwork
+            login_kwork, passwork_kwork = message.text.split(" ")
+            await bot.send_message(message.from_user.id, "")
+    else:
+        await bot.send_message(message.from_user.id, "Не, это не для тебя")
+
+
+@dp.message_handler(commands=["answer"])
+async def text(message: types.Message):
+    markup = types.InlineKeyboardMarkup()
+    for user_name, user_id in unread_names_ids.items():
+        markup.add(types.InlineKeyboardButton(user_name, callback_data=user_id))
+    await bot.send_message(ADMIN_ID, "Выбери кому ответить", reply_markup=markup)
+
+
+@dp.callback_query_handler(lambda user_id: user_id.data in unread_names_ids.values())
+async def answer(message: types.Message):
+    await bot.send_message(ADMIN_ID, "Напиши сообщение")
+
+    @dp.message_handler(content_types=["text"])
+    async def send_answer(answer: types.Message):
+        api = Kwork(login=login_kwork, password=passwork_kwork)
+        await api.send_message(user_id=int(message.data), text=answer.text)
+        await api.close()
+        await bot.send_message(ADMIN_ID, "Отправлено")
+        # Удаление непрочитанного
+        for user_name, user_id in unread_names_ids.items():
+            if int(message.data) == user_id:
+                del unread_names_ids[user_name]
+
+
+async def get_unread_messages():
+    while True:
+        api = Kwork(login=login_kwork, password=passwork_kwork)
+
+        # Если "Необходимо ввести последние 4 цифры номера телефона."
+        # api = Kwork(login="login", password="password", phone_last="0102")
+
+        # Получения всех диалогов на аккаунте
+        all_dialogs = await api.get_all_dialogs()
+        for line in all_dialogs:
+            if line.unread_count > 0:
+                if line.user_id not in unread_names_ids.values():
+                    unread_names_ids[line.username] = str(line.user_id)
+                await bot.send_message(ADMIN_ID,
+                                       f"{line.username} ({line.unread_count}): {line.last_message}\nОтветить: /answer")
+        await api.close()
+        await asyncio.sleep(600)
+
+
+if __name__ == '__main__':
+    dp.loop.create_task(get_unread_messages())
+    executor.start_polling(dp)
